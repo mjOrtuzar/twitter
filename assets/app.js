ea7375aad42f76d3ecf108ad7a36962cc50abec8
mjOrtuzar <re4ortuzar@hotmail.com>
2017-11-12 14:52:15 -0300

    actualizacion twitter

--- assets/app.js
+++ assets/app.js
@@ -1,30 +1,33 @@
  //funcionamiento general de la pagina antes del css
-
-
 window.onload=function(){
    var sendButton= document.getElementsByName("send")[0];  //getElements toma los elementos que coincidan con el Name.
-   var twitInput=document.getElementsByName("input")[0];   //y tomara el primer arreglo que aparesca ([0]);
+   var twitInput=document.getElementsByName("input")[0]; //textArea  //y tomara el primer arreglo que aparesca ([0]);
    var timeLine=document.getElementsByName("timeL")[0];
- //definen variable para contar caracteres
-   var longi=140;
-   var resto="";
-   var obj= document.getElementById('input').value.length;
-   resto=longi-obj;
-   //funcion contador
-   twitInput.onkeyup=function cuenta(){
-     if(resto == 140){
-      sendButton.disabled=true;
-      document.getElementById("contar").style.color=red;
-     }else if(resto >130){
-      document.getElementById("contar").style.color=yellow;
+   var counter=document.getElementById("contar");
 
-     }else if(resto >120){
-      document.getElementById("contar").style.color=orange;
-    }
-    document.getElementById('contar').value=resto;
-}
+ 
+   //funcion contador
+ twitInput.onkeyup=function(){
+   var charLong= 140;
+   var resto="";
+   var think= twitInput.value.length;
+   resto=charLong-think;
+   counter.value=resto;
+   if(resto >120 && resto<140){
+    sendButton.disabled=false;
+    counter.style.color="yellow";
+
+   }else if(resto <120 && resto >0){
+      sendButton.disabled=false;
+      counter.style.color="orange";
+   }else if(resto < 0){
+      sendButton.disabled =true;
+      counter.style.color="red";
+   }
+
+ }
   //funcion click
-   sendButton.onkeyup=function(){ //onkeypress es un evento que hara algo cuando el boton en el html se aprete;
+   sendButton.onclick=function(){ //onkeypress es un evento que hara algo cuando el boton en el html se aprete;
    	if(twitInput.value === ""){          //primera condicion si no se ingresa nada el boton queda deshabilitado.
    		sendButton.disabled=true;
    	}else{
@@ -33,13 +36,13 @@ window.onload=function(){
    		var nSpan= document.createElement('span');
    		var dSpan= document.createElement('span');
    		var tParr= document.createElement('p');
-   		tDiv.appendChild(nSpan);
+         tDiv.appendChild(nSpan);
    		tDiv.appendChild(dSpan);
    		tDiv.appendChild(tParr);
    		tDiv.className="tuit";
    		dSpan.textContent= new Date();
    		tParr.textContent= twitInput.value;
-   		timeLine.insertBefore(tDiv, timeLine.children[0]);
+         timeLine.insertBefore(tDiv, timeLine.children[0]);
 
    	}
 
