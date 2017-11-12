2e76b459756cc57ba1bfa46427ed55a84bd58d13
mjOrtuzar <re4ortuzar@hotmail.com>
2017-11-12 02:06:36 -0300

    actualizacion twitter

--- assets/app.js
+++ assets/app.js
@@ -5,7 +5,26 @@ window.onload=function(){
    var sendButton= document.getElementsByName("send")[0];  //getElements toma los elementos que coincidan con el Name.
    var twitInput=document.getElementsByName("input")[0];   //y tomara el primer arreglo que aparesca ([0]);
    var timeLine=document.getElementsByName("timeL")[0];
-   sendButton.onkeypress=function(){ //onClick es un evento que hara algo cuando el boton en el html se aprete;
+ //definen variable para contar caracteres
+   var longi=140;
+   var resto="";
+   var obj= document.getElementById('input').value.length;
+   resto=longi-obj;
+   //funcion contador
+   twitInput.onkeyup=function cuenta(){
+     if(resto == 140){
+      sendButton.disabled=true;
+      document.getElementById("contar").style.color=red;
+     }else if(resto >130){
+      document.getElementById("contar").style.color=yellow;
+
+     }else if(resto >120){
+      document.getElementById("contar").style.color=orange;
+    }
+    document.getElementById('contar').value=resto;
+}
+  //funcion click
+   sendButton.onkeyup=function(){ //onkeypress es un evento que hara algo cuando el boton en el html se aprete;
    	if(twitInput.value === ""){          //primera condicion si no se ingresa nada el boton queda deshabilitado.
    		sendButton.disabled=true;
    	}else{
@@ -18,11 +37,13 @@ window.onload=function(){
    		tDiv.appendChild(dSpan);
    		tDiv.appendChild(tParr);
    		tDiv.className="tuit";
-   		nSpan.textContent= "Lab";
    		dSpan.textContent= new Date();
    		tParr.textContent= twitInput.value;
    		timeLine.insertBefore(tDiv, timeLine.children[0]);
+
    	}
-   }
+
+
+   } 
 
 }
